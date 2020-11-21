d980fa73bc555e57ea80ecff10cdf786925b46f1
Max Hanson <mthanson2@gmail.com>
2020-11-21 15:04:11 -0800

    Fixed file structure

--- server.js
+++ server.js
@@ -15,8 +15,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useFindAndModify: false
 });
 
-// routes
-// app.use(require("./routes/api.js"));
+app.use(require("./routes/api"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
